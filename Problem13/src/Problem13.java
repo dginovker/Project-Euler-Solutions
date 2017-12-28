92fa908aec63e21b534951ae3bb8652547a176f6
Before <dcress01@mail.uoguelph.ca>
2017-12-28 17:57:22 -0500

    Started problem 13

---
+++ Problem13/src/Problem13.java
@@ -0,0 +1,13 @@
+/*
+    Problem 13
+    Found on https://projecteuler.net/problem=13
+    Solution by Daniel Ginovker
+    ABCms solution
+ */
+
+
+public class Problem13 {
+    public static void main(String[] args) {
+        
+    }
+}
