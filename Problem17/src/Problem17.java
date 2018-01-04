533807143c431475d79e52d46ee3ee0ed4f4fa8d
Before <dcress01@mail.uoguelph.ca>
2018-01-04 15:54:11 -0500

    Finished problem 17 - 0ms solution

--- Problem17/src/Problem17.java
+++ Problem17/src/Problem17.java
@@ -3,43 +3,34 @@
     Found on https://projecteuler.net/problem=17-
     Solution by Daniel Ginovker
     0ms solution
+    Notes: This is a disgusting problem
  */
 
-
 public class Problem17 {
-    public static final int UPTOTWENTYVALS[] = {4, 3, 3, 5, 4, 3, 5, 5, 4, 6, 6, 8, 8, 7, 7, 9, 8, 8, 6};
-    public static final int TENS[] = {3, 6, 6, 5, 5, 5, 7, 7, 6};
-    public static final int ONE = 3, TWO = 3, THREE = 5, FOUR = 4, FIVE = 4, SIX = 3, SEVEN = 5, EIGHT = 5, NINE = 4;
-    public static final int ONETONINE = 36;
-    public static final int TENTONINETEEN = 73;//3 + eleven 6 + twelve 6 + thirteen 8 + fourteen 8 + fifteen 7 + sixteen 7 + seventeen 9 + eighteen 8 + 8
-    public static final int TWENTY = 6, THIRTY = 6, FORTY = 5, FIFTY = 5;
+    public static final int UPTOTWENTYVALS[] = {0, 3, 3, 5, 4, 4, 3, 5, 5, 4, 3, 6, 6, 8, 8, 7, 7, 9, 8, 8, 6};
+    public static final int TENS[] = {0, 3, 6, 6, 5, 5, 5, 7, 6, 6};
     public static final int HUNDRED = 7;
     public static final int THOUSAND = 8;
     public static final int AND = 3;
+
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
 
         int sum = 0;
 
-        int oneToNineteen = ONETONINE + TENTONINETEEN;
-        int twentyToNinetynine = (TWENTY + THIRTY + FORTY + FIFTY + (SIX + SEVEN + EIGHT + NINE + 4*2))*10 + 8*ONETONINE;
-        int oneToNinetynine = oneToNineteen + twentyToNinetynine;
-
-        int hundreds = ONETONINE + HUNDRED*9;
-
-        sum += oneToNinetynine; //1 - 99
-        sum += ((HUNDRED + AND)*99 + oneToNinetynine)*9; //101 - 999. hundreds not included
-        sum += hundreds; //hundreds
-        sum += ONE + THOUSAND;//thousand
+        for (int i = 1; i <= 1000; i++)
+        {
+            sum += getCharCount(i);
+        }
 
         long stop = System.currentTimeMillis();
         System.out.println("The sum of the letters from one to one thousand is " + sum);
         System.out.println("Time: " + (stop-start) + "ms");
     }
 
-    int getCharCount(int i)
+    private static int getCharCount(int i)
     {
-        if (i <= 9)
+        if (i <= 19)
         {
             return UPTOTWENTYVALS[i];
         }
@@ -49,7 +40,14 @@ public class Problem17 {
         }
         if (i <= 999)
         {
-            return 
+            int notFirst = i%100;
+            if (notFirst == 0)
+            {
+                return UPTOTWENTYVALS[i/100] + HUNDRED;
+            }
+            return UPTOTWENTYVALS[i/100] + (HUNDRED + AND) + getCharCount(notFirst);
+            //Return (X hundred*yy) + getCharCount(yy), where YY is YY as seen in xYY
         }
+        return UPTOTWENTYVALS[1] + THOUSAND;
     }
 }
